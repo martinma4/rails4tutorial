cd45fb7706dae8777bc844e654dd2c6577be79b6
martin ma <martin.maqianli@gmail.com>
2016-05-09 20:30:04 +1000

    finish log-in-log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
