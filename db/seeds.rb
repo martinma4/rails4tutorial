adf61f65c8c1836923235525fa8266d1344d3150
martin ma <martin.maqianli@gmail.com>
2016-05-16 20:54:34 +1000

    finish work listing 10.1.1 on May, 16

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name:  "Hello World",
              email: "hello@world.com",
              password:              "helloworld",
              password_confirmation: "helloworld",
-             admin: true)
+             admin: true
+             activated: true
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -11,5 +13,7 @@ User.create!(name:  "Hello World",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password
+               activated: true,
+               activated_at: Time.zone.now)
 end
