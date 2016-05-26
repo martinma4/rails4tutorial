a581e7a377ebfa41de8a47424c49552603a258a6
martin ma <martin.maqianli@gmail.com>
2016-05-26 21:40:23 +1000

    add region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJ2QSUDP3NTLHTSMA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
     config.fog_directory     =  ENV['martinma4']
   end
